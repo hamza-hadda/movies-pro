0db331d1b7bcee3657a3921a029e447e3f5613ef
Hamza.Haddaa <hamza.1996sfr@gmail.com>
2024-06-03 23:44:13 +0100

    finish the front application with listing all the fields

---
+++ movies_web/src/views/MovieList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Movies</h1>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.title }} - {{ movie.description }}
+        <ul>
+          <li v-for="actor in movie.actors" :key="actor.id">
+            {{ actor.first_name }} {{ actor.last_name }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from '../axios';
+
+export default {
+  data() {
+    return {
+      movies: []
+    };
+  },
+  created() {
+    axios.get('movies/')
+      .then(response => {
+        this.movies = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
